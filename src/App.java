af503029650389ac4794631a673ac380640e9a56
Angelinee26 <ngelineee.e@gmail.com>
2022-05-31 22:20:41 +0700

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+class App {
+    public static void main (String[] args) throws Exception {
+        String pesan = "Selamat Datang di Mata Kuliah Pemrograman Berorientasi Objek";
+        int tahun = 2022;
+        System.out.println (pesan + " Tahun " + tahun);
+
+    }
+}
