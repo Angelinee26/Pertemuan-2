2812fdc3d122784f78a19f6f5d4d2803f5520a8b
Angelinee26 <ngelineee.e@gmail.com>
2022-06-01 17:34:05 +0700

    tugas

--- src/App.java
+++ src/App.java
@@ -1,8 +1,28 @@
-class App {
-    public static void main (String[] args) throws Exception {
-        String pesan = "Selamat Datang di Mata Kuliah Pemrograman Berorientasi Objek";
-        int tahun = 2022;
-        System.out.println (pesan + " Tahun " + tahun);
+import bangunruang.Tabung;
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) throws Exception {
+        String pesan = "Selamat Datang di Mata Kuliah \" Pemrograman Berorientasi Objek \" ";
+        int tahun = 2022;
+
+        System.out.println("***************************************************************************");
+        System.out.println(pesan + " Tahun " + tahun);
+        System.out.println("***************************************************************************");
+        
+    //Scan inputan keyboard
+    Scanner keyboard = new Scanner(System.in);
+    
+    Tabung tabung = new Tabung();
+    System.out.println("Input Jari-jari = ");
+    tabung.r = keyboard.nextInt();
+    System.out.println("Input Tinggi = ");
+    tabung.t = keyboard.nextInt();
+
+    //tampilkan volume tabung
+    tabung.tampilkanVolumeTabung();
+
+    keyboard.close();
     }
+    
 }
